2f90143e9a263971017f8bceb5685537bf466f64
Joan Catala <joan@riseup.net>
2019-01-21 13:02:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -96,14 +96,5 @@ function doscolumnas_exclusivo() {
 	</div>
 	<img id="bottom" src="bottom.png" alt="">
 
-
-<!-- JAVASCRIPT IRRELEVANT (esborrem en uns dies)
-
-<script src="js/jquery-1.js"></script> 
-<script src="js/bootstrap.js"></script>
-<script src="js/jquery-ui.js"></script>
-
- -->
-
 	</body>
 </html>
